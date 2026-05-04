efa90d8e5b99cb19b75520eb75b63ea2db540ef1
Ben Baker <bakerb845@gmail.com>
2026-05-04 15:05:37 -0600

    cleaned up some clang-tidy warnings

--- testing/grpc.cpp
+++ testing/grpc.cpp
@@ -1,36 +1,33 @@
 #include <cmath>
+#include <cstdint>
+#include <optional>
 #include <string>
-#include <chrono>
-#include <bit>
 #include <catch2/catch_test_macros.hpp>
-#include <catch2/catch_template_test_macros.hpp>
-#include <catch2/catch_approx.hpp>
-#include <catch2/benchmark/catch_benchmark.hpp>
-#include <catch2/matchers/catch_matchers_floating_point.hpp>
 #include "grpcOptions.hpp"
 
 TEST_CASE("UPacketImportProxy", "[grpcOptions]")
 {
     SECTION("Defaults")
     {
-        UDataPacketImportProxy::GRPCOptions options;
+        const UDataPacketImportProxy::GRPCOptions options;
         REQUIRE(options.getHost() == "localhost");
         REQUIRE(options.getPort() == 50000);
         REQUIRE(options.getAccessToken() == std::nullopt);
         REQUIRE(options.getServerCertificate() == std::nullopt);
         REQUIRE(options.getServerKey() == std::nullopt);
         REQUIRE(options.getClientCertificate() == std::nullopt);
+        REQUIRE(options.reflectionEnabled() == false);
     }
 
     SECTION("Options")
     {
-        std::string host{"some.host.org"};
-        std::string token{"super-secret-token"};
-        std::string serverCertificate{"some-wonky-hash"};
-        std::string serverKey{"some-private-wonky-hash"};
-        std::string clientCertificate{"some-other-hash"};
-        std::string clientKey{"some-private-hash"};
-        uint16_t port{12345};
+        const std::string host{"some.host.org"};
+        const std::string token{"super-secret-token"};
+        const std::string serverCertificate{"some-wonky-hash"};
+        const std::string serverKey{"some-private-wonky-hash"};
+        const std::string clientCertificate{"some-other-hash"};
+        //const std::string clientKey{"some-private-hash"};
+        constexpr uint16_t port{12345};
         UDataPacketImportProxy::GRPCOptions options;
 
         options.setHost(host);
@@ -39,12 +36,17 @@ TEST_CASE("UPacketImportProxy", "[grpcOptions]")
         options.setServerKey(serverKey);
         options.setAccessToken(token);
         options.setClientCertificate(clientCertificate);
+        options.enableReflection();
 
         REQUIRE(options.getHost() == host);
         REQUIRE(options.getPort() == port);
-        REQUIRE(*options.getServerCertificate() == serverCertificate);
-        REQUIRE(*options.getServerKey() == serverKey);
-        REQUIRE(*options.getAccessToken() == token);
-        REQUIRE(*options.getClientCertificate() == clientCertificate);
+        REQUIRE(options.getServerCertificate() != std::nullopt);
+        REQUIRE(options.getServerKey() != std::nullopt);
+        REQUIRE(options.getAccessToken() != std::nullopt);
+        REQUIRE(*options.getServerCertificate() == serverCertificate); //NOLINT
+        REQUIRE(*options.getServerKey() == serverKey); //NOLINT
+        REQUIRE(*options.getAccessToken() == token); //NOLINT
+        REQUIRE(*options.getClientCertificate() == clientCertificate); //NOLINT
+        REQUIRE(options.reflectionEnabled() == true);
     }
 }
